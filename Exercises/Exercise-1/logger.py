a50bcd3a6d4cfb661d533cc607cacd1c8c4b9df7
Amirh14123 <aa940671@gmail.com>
2024-09-29 16:14:08 +0330

    exercise-1 first sync

---
+++ Exercises/Exercise-1/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+# Basic logger setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("main.log"),
+        logging.StreamHandler()
+    ]
+)
+logger =  logging.getLogger()
--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,8 @@
 import requests
+from logger import logger
+from os.path import join,exists
+from os import mkdir
+from zipfile import ZipFile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,10 +15,46 @@ download_uris = [
 ]
 
 
-def main():
-    # your code here
-    pass
+def download_files(uri:str,dest_folder:str) -> str | None:
+    logger.info("Starting the download...")
+    logger.info(f"URI : {uri}")
+    file_name = uri.split("/")[-1]
+    file_path = join(dest_folder, file_name) # os path join 
+    try:
+        with requests.get(uri,stream=True,timeout=10) as response:
+            response.raise_for_status() # Checks for status codes
+            logger.info("Download Completed")
+            with open(file_name, 'wb') as file:
+                for text in response.iter_content(chunk_size=4096):
+                    file.write(text)
+        logger.info(f"File Created: {file}")
+        return file_name
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return None
+    
+def unzip(file_name:str , dest_folder:str) -> None:
+    try:
+        with ZipFile(file_name) as zip_file:
+            logger.info(f"Unzipping {file_name}")
+            zip_file.extractall(dest_folder)
+        logger.info("Extracted.")
+    except Exception as e:
+        logger.error(f"Error: {e}")
 
+    except:...
+def downloader(uri_list:list,dest_folder:str) -> None:
+    if not exists(dest_folder):
+        mkdir(dest_folder)
+
+    for uri in uri_list:
+        download_files(uri,dest_folder)
+
+
+
+
+def main():
+    downloader(download_uris,'./downloads')
 
 if __name__ == "__main__":
     main()
